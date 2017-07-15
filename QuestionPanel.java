571646863659ce3c18461bbf697aa1fa3f6d3846
TinyZhen <luz@mail.gvsu.edu>
2017-07-15 14:51:16 -0400

    update

--- QuestionPanel.java
+++ QuestionPanel.java
@@ -11,7 +11,9 @@ import javax.swing.border.Border;
 
 public class QuestionPanel extends JFrame implements ActionListener,KeyListener {
 
-	private JLabel question;
+	private JTextArea question;
+	private JLabel playerterm;
+	private int[] player;
 	private JPanel Qpanel;
 	private JPanel Cpanel;
 	private JFrame frame;
@@ -20,31 +22,41 @@ public class QuestionPanel extends JFrame implements ActionListener,KeyListener 
 	private JRadioButton choiceB;
 	private JRadioButton choiceC;
 	private JRadioButton choiceD;
+	private JButton submit;
 	private  ButtonGroup bG;
-	Questions q;
+	QGenerator q;
 	Countdown count;
 	
-	public QuestionPanel(){
+	public QuestionPanel(int index){
 		JOptionPane.showMessageDialog(frame,"Player1 press z player 2 press m");
-		q = new Questions();
+		player = new int[2];
+		q = new QGenerator();
+		q.createQSet();
 		frame = new JFrame();
 		Qpanel = new JPanel();
 		Cpanel = new JPanel();
 		bG = new ButtonGroup();
-		
-		
+		playerterm = new JLabel();
+		submit = new JButton();
+		submit.addActionListener(this);
+
 		Font font1 = new Font("Tahoma", Font.CENTER_BASELINE, 18);
 		
 		
-		question = new JLabel(q.getQue());
+		question = new JTextArea(10,40);
 		question.setFont(font1);
+		question.setText(q.getQuestionAt(index).getQue());
+		question.setLineWrap(true);
+		question.setWrapStyleWord(true);
+		question.setBackground(Color.LIGHT_GRAY);
+		question.setEditable(false);
 		timer = new JLabel();
 
 		
-		choiceA = new JRadioButton(q.getAChoice());
-		choiceB = new JRadioButton(q.getBChoice());
-		choiceC = new JRadioButton(q.getCChoice());
-		choiceD = new JRadioButton(q.getDChoice());
+		choiceA = new JRadioButton(q.getQuestionAt(index).getAChoice());
+		choiceB = new JRadioButton(q.getQuestionAt(index).getBChoice());
+		choiceC = new JRadioButton(q.getQuestionAt(index).getCChoice());
+		choiceD = new JRadioButton(q.getQuestionAt(index).getDChoice());
 		choiceA.addActionListener(this);
 		choiceB.addActionListener(this);
 		choiceC.addActionListener(this);
@@ -64,18 +76,19 @@ public class QuestionPanel extends JFrame implements ActionListener,KeyListener 
 		frame.add(Qpanel,BorderLayout.NORTH);
 		frame.add(Cpanel,BorderLayout.SOUTH);		
 	    frame.pack();
-	    frame.setSize(600, 200);
+	    frame.setSize(1000, 600);
 	    frame.setLocationRelativeTo(null);
+	    frame.add(submit);
+	    
 	    frame.setVisible(true);
 	    choiceA.setEnabled(false);
 	    choiceB.setEnabled(false);
 	    choiceC.setEnabled(false);
 	    choiceD.setEnabled(false);
-	    
 	    frame.addKeyListener(this); 
 	    frame.setFocusable(true);
-	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+	    //frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    
 	    }
 	    
 	@Override
@@ -89,6 +102,7 @@ public class QuestionPanel extends JFrame implements ActionListener,KeyListener 
     	    choiceB.setEnabled(true);
     	    choiceC.setEnabled(true);
     	    choiceD.setEnabled(true);
+    	    playerterm.setText("Player 1");
     	    
      	}
      	 if(e.getKeyCode() == KeyEvent.VK_M){
@@ -96,8 +110,16 @@ public class QuestionPanel extends JFrame implements ActionListener,KeyListener 
     	    choiceB.setEnabled(true);
     	    choiceC.setEnabled(true);
     	    choiceD.setEnabled(true);
+    	    playerterm.setText("Player 2");
      	}
-     		 
+     		
+     	 if(e.getKeyCode() == KeyEvent.VK_Q){
+     	    choiceA.setEnabled(true);
+     	    choiceB.setEnabled(true);
+     	    choiceC.setEnabled(true);
+     	    choiceD.setEnabled(true);
+     	    playerterm.setText("Player 3");
+      	}
      }
 	 
 	 @Override
@@ -108,12 +130,18 @@ public class QuestionPanel extends JFrame implements ActionListener,KeyListener 
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getSource()==submit){
+			player[0]=100;
+		}
 	}
 	
-	public static void main(String arg[]) {
-		QuestionPanel gui = new QuestionPanel();
+	public int getPlayerScore(){
+		return player[0];
+	}
 
+	
+	public static void main(String arg[]) {
+		QuestionPanel gui = new QuestionPanel(10);
 
 	}
 
